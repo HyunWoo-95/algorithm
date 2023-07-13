9489229bd2105e87e413c6058642debbad6e372a
hyunwoo <dayou346@gmail.com>
2023-07-13 10:50:37 +0900

    2023-07-13

---
+++ algorithm/src/main/java/org/example/back/Main00.java
@@ -0,0 +1,8 @@
+package org.example.back;
+
+public class Main00 {
+    public static void main(String[] args) {
+        String result = "Hello World!";
+        System.out.println(result);
+    }
+}
---
+++ algorithm/src/main/java/org/example/back/Main01.java
@@ -0,0 +1,20 @@
+package org.example.back;
+
+import java.util.Scanner;
+
+public class Main01 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+        int result = a + b;
+        System.out.println(result);
+
+    }
+    /*
+    Scanner 사용 방법을 알지 못함.
+    1. 먼저 Scanner 객체명 = new Scanner(System.in) 선언해야하며 System.in 필수로 넣어줘야함
+    2.System.in 은 사용자로부터 입력을 받기 위한 입력 스트림이다.
+        그렇기 때문에 Scanner 뿐만 아니라 다른 입력 방식들도 사용자로부터 입력을 받기 위해서는 System.in 이 들어간다.
+     */
+}
