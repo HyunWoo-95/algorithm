311cf0d4dbea8cbb6f81e575b2d546928c9d3641
hyunwoo <dayou346@gmail.com>
2023-07-15 10:03:57 +0900

    2023-07-14

---
+++ algorithm/src/main/java/org/example/back/Main06.java
@@ -0,0 +1,17 @@
+package org.example.back;
+
+import java.util.Scanner;
+
+public class Main06 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
---
+++ algorithm/src/main/java/org/example/back/Main07.java
@@ -0,0 +1,15 @@
+package org.example.back;
+
+import java.util.Scanner;
+
+public class Main07 {
+    public static void main(String[] args) {
+        String EID = null;
+        Scanner scr = new Scanner(System.in);
+        String imapct = "??!";
+        String id = scr.next();
+        if (id.equals(EID)) {
+            System.out.println(id + imapct);
+        }
+    }
+}
