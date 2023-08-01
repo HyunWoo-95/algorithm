6fa37b0953a07d15f99da7f31a66171ee8945f88
hyunwoo <dayou346@gmail.com>
2023-08-01 09:53:46 +0900

    2023.08.01

--- algorithm/src/main/java/org/example/back/print/Main10.java
+++ algorithm/src/main/java/org/example/back/print/Main10.java
@@ -1,16 +1,21 @@
 package org.example.back.print;
 
 import java.util.Scanner;
-// 런타임에러 (StringIndexOutOfBoundsException)
 public class Main10 {
     public static void main(String[] args) {
-        Scanner scr = new Scanner(System.in);
-        int tc = scr.nextInt();
-        String str = scr.nextLine();
-        int result = 0;
-        for (int i = 0; i < tc; i++) {
-            result += str.charAt(i) - '0';
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine(); // 다음 줄로 포인터 이동
+
+        String str = sc.nextLine(); // 문자열을 받음
+
+        int sum = 0; // 숫자의 합계를 저장할 변수
+
+        for (int i = 0; i < n; i++) {
+            sum += str.charAt(i) - '0'; // charAt으로 해당 인덱스의 문자열을 char 타입으로 바꿈. char 타입끼리 연산하여 int형에 저장하면 아스키코드 값을 기반으로 연산하기 때문에 문자열 0을 빼줌. (아스키코드 0이 48인가 그렇습니다)
         }
-        System.out.println(result);
+
+        System.out.println(sum);
     }
 }
