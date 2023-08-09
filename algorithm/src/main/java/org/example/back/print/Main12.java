2e73f45fd6d3a978f304f478fe30ad4a82a6a00f
hyunwoo <dayou346@gmail.com>
2023-08-09 19:19:28 +0900

    2023.08.09 : N 찍기

---
+++ algorithm/src/main/java/org/example/back/print/Main12.java
@@ -0,0 +1,14 @@
+package org.example.back.print;
+
+import java.util.Scanner;
+public class Main12 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+        int n = scr.nextInt();
+        for (int i = 1; i<=n; i++){
+            System.out.println(i);
+        }
+
+    }
+}
