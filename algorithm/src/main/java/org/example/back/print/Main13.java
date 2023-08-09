d76bb7a39264d8fc65a31032002e52066e93bcbe
hyunwoo <dayou346@gmail.com>
2023-08-09 19:29:14 +0900

    2023.08.09 : 기찍 N

---
+++ algorithm/src/main/java/org/example/back/print/Main13.java
@@ -0,0 +1,12 @@
+package org.example.back.print;
+
+import java.util.Scanner;
+public class Main13 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
+        for (int i = n; i >=1; i--) {
+            System.out.println(i);
+        }
+    }
+}
