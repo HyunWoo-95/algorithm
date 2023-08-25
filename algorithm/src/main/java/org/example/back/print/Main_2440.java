aed4022ba118663216055c40dad04324f0619ac8
hyunwoo <dayou346@gmail.com>
2023-08-25 17:25:58 +0900

    2023.08.25 : 알고리즘

---
+++ algorithm/src/main/java/org/example/back/print/Main_2440.java
@@ -0,0 +1,22 @@
+package org.example.back.print;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = a; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
