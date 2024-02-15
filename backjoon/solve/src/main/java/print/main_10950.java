86761ad3c98747a98756ebeb4986309e56d7f903
JoHyunwoo <jhw3874@gmail.com>
2024-02-15 18:37:49 +0900

    ps 500 연습 1일차

---
+++ backjoon/solve/src/main/java/print/main_10950.java
@@ -0,0 +1,20 @@
+package print;
+
+import java.util.Scanner;
+
+public class main_10950 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+
+    for (int i = 0; i <= t; i++) {
+      int a, b;
+      a = sc.nextInt();
+      b = sc.nextInt();
+      System.out.println(a + b);
+
+    }
+  }
+}
---
+++ backjoon/solve/src/main/java/print/main_2557.java
@@ -0,0 +1,9 @@
+package print;
+
+import static java.lang.System.*;
+public class main_2557 {
+  public static void main(String[] args) {
+    String s = "Hello World!";
+    out.println(s);
+  }
+}
