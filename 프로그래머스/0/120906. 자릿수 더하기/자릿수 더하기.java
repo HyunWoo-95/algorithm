78098802f84b22336d65b92b838c057aac66fa3f
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2024-11-12 21:21:14 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.21 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,8 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n) {
-        String[] arr = String.valueOf(n).split("");
-        return Arrays.stream(arr).mapToInt(Integer::parseInt).sum();
-      
+//         String[] arr = String.valueOf(n).split("");
+//         return Arrays.stream(arr).mapToInt(Integer::parseInt).sum();
+        int answer = 0;
+        String[] s = String.valueOf(n).split("");
+        for(int i = 0; i < s.length; i++){
+            answer += Integer.parseInt(s[i]);
+        }
+        return answer; 
     }
 }
