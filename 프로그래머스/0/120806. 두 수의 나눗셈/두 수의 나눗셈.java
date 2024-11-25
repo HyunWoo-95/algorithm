c077b3f0267924bffd804337fb6ee2b52df35a82
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2024-11-26 00:04:37 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -2,9 +2,10 @@ class Solution {
     public int solution(int num1, int num2) {
         // 형변환 사용 Int -> double -> int 순서
         // 정수형으로 계산 시 맞아떨어지지 않는 계산은 소수점 기준으로 절삭된다.
-        int answer =  0;
-        double res = (double) num1 / num2 * 1000;
-        answer = (int) res;
-        return answer;
+        // int answer =  0;
+        // double res = (double) num1 / num2 * 1000;
+        // answer = (int) res;
+        // return answer;
+        return num1 * 1000 / num2;
     }
 }
