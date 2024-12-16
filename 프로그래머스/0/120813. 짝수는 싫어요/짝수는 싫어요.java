95fe9e56c7de0bbb0c3ede5f475e166b7148c835
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2024-12-16 15:53:45 +0900

    [level 0] Title: 짝수는 싫어요, Time: 4.84 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,14 +1,16 @@
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n) {
-        int[] answer;
-        if(n % 2 == 0){
-            answer = new int[n/2];
-        }else {
-            answer = new int[n/2 +1];
-        }
-        for(int i = 0; i < answer.length; i++){
-                answer[i] = (i*2)+1;
-        }
-        return answer;
+        // int[] answer;
+        // if(n % 2 == 0){
+        //     answer = new int[n/2];
+        // }else {
+        //     answer = new int[n/2 +1];
+        // }
+        // for(int i = 0; i < answer.length; i++){
+        //         answer[i] = (i*2)+1;
+        // }
+        // return answer;
+        return IntStream.rangeClosed(0,n).filter(value -> value % 2 == 1).toArray();
     }
 }
