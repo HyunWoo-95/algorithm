c9151d314d127fe33907abd31d224c629bcf0f3b
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2025-01-07 00:18:08 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,14 +2,22 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-        while(n >0){
-            if(n % 2== 1){
-            ans+=1;
-            }
-              n/=2;
-        }
+        /**
+         *N이 500일 때
+         - 500-> 25-> 125(ans + 1)-> 62-> 31(ans + 1)-> 15(ans + 1)-> 7(ans + 1)-> 3(ans + 1)-> 1(ans + 1)              - 따라서 건전지 사용량은 6이 된다.
+         - n / 2가 홀수일 때만 ans에 1을 더해준다.
+         - 이 과정을 n > 0이 될 때까지 반복한다.
+        */
 
-        return ans;
+//         int ans = 0;
+//         while(n >0){
+//             if(n % 2== 1){
+//             ans+=1;
+//             }
+//               n/=2;
+//         }
+
+//         return ans;
+        return Integer.bitCount(n);
     }
 }
