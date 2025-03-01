efc91266c9e0abd87f94eeca209258bee5a4b21e
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2025-03-01 15:22:54 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,10 +1,15 @@
 def solution(rsp):
     answer = ""
+    # for i in rsp:
+    #     if i == '2':
+    #         answer += '0'
+    #     elif i == '0':
+    #         answer += '5'
+    #     elif i == '5':
+    #         answer += '2'
+    # return answer
+    
+    result = {'2':'0', '0':'5','5':'2'}
     for i in rsp:
-        if i == '2':
-            answer += '0'
-        elif i == '0':
-            answer += '5'
-        elif i == '5':
-            answer += '2'
+        answer += result.get(i)
     return answer
