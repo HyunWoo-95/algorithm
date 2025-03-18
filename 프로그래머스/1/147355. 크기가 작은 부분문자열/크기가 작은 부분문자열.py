242032cede0f62ab77c0ebea83769a73e88081d3
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2025-03-18 12:53:08 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 3.64 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,10 @@
+
+
+def solution(t, p):
+    answer = 0
+    len_p = len(p) # '271' = 3
+    int_p = int(p)
+    for i in range(len(t) - len_p + 1): # 연속된 부분 문자열 탐색
+        if int(t[i:i+len_p]) <= int_p: # 
+            answer += 1
+    return answer
