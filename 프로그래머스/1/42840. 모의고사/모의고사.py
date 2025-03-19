3f36225e7c6f872a5d850716b4e0b5b01a5248d7
Hyunwoo <108071860+Da-you@users.noreply.github.com>
2025-03-19 11:32:50 +0900

    [level 1] Title: 모의고사, Time: 6.60 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,17 @@
+def solution(answers):
+    answer = []
+    supoja_1 = [1, 2, 3, 4, 5]
+    supoja_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    supoja_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    scores = [0,0,0]
+    for i in range(len(answers)):
+        if answers[i] == supoja_1[i % len(supoja_1)]:  # 패턴 반복
+            scores[0] += 1
+        if answers[i] == supoja_2[i % len(supoja_2)]:
+            scores[1] += 1
+        if answers[i] == supoja_3[i % len(supoja_3)]:
+            scores[2] += 1
+    max_score = max(scores)
+    
+    result = [i + 1 for i, score in enumerate(scores) if score == max_score]
+    return result
