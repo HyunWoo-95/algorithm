7b3f20729d94d549dff0b9f70b9449a43445f9d1
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-07-25 15:11:42 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,12 +1,18 @@
 def solution(array):
-    answer = [0] * (max(array)+1)
-    for i in array :
-        answer[i] += 1
-    m = 0
-    for c in answer :
-        if c == max(answer) :
-            m += 1
-    if m != 1 :
-        return -1
-    else :
-        return answer.index(max(answer))
+    # answer = [0] * (max(array)+1) # 숫자 출연의 빈도 수를 기록할 리스트
+    # for i in array :
+    #     answer[i] += 1
+    # m = 0 # 최빈값의 개수
+    # for c in answer :
+    #     if c == max(answer) :
+    #         m += 1
+    # if m != 1 :
+    #     return -1
+    # else :
+    #     return answer.index(max(answer))
+    while len(array) != 0 :
+        for i, a in enumerate(set(array)) : 
+            # i는 index, a는 값
+            array.remove(a)
+        if i == 0 : return a
+    return -1
