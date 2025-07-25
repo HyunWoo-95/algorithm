03026326e239e5f61939f1ab938ea5c2fb82735a
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-07-25 14:33:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 14.67 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,9 +1,12 @@
 def solution(array):
-    arr = [0] * 1001 # 배열에 존재하는 값을 저장하기 위한 배열
-    
-    for i in range(len(array)): # 입력값의 값을 인덱스로 이용해 동일한 값마다 +1
-        arr[array[i]] += 1
-    # 배열의 최빈값이 1개 이상일 경우 -1 반환
-    if arr.count(max(arr)) > 1:
+    answer = [0] * (max(array)+1)
+    for i in array :
+        answer[i] += 1
+    m = 0
+    for c in answer :
+        if c == max(answer) :
+            m += 1
+    if m != 1 :
         return -1
-    return arr.index(max(arr))
+    else :
+        return answer.index(max(answer))
