12e0a7ffbd90270393ffef103344f14106348620
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-11 23:16:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.55 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,10 +1,13 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        int answer1 = numbers[0] * numbers[1];
-        int answer2 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+//         Arrays.sort(numbers);
+//         int answer1 = numbers[0] * numbers[1];
+//         int answer2 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
         
-        return answer1 > answer2 ? answer1 : answer2;
+//         return answer1 > answer2 ? answer1 : answer2;
+        //  두 번째 풀이
+         Arrays.sort(numbers);
+         return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
     }
 }
