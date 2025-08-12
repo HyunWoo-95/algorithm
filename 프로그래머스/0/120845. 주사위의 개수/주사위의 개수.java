e72c3197aed5fca6bb755b8637888f03cb481395
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-12 23:38:42 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,10 +1,15 @@
 class Solution {
     public int solution(int[] box, int n) {
         int answer = 0;
-        int n1 = box[0] / n;
-        int n2 = box[1] / n;
-        int n3 = box[2] / n;
-        answer = n1 * n2 * n3;
+        // int n1 = box[0] / n;
+        // int n2 = box[1] / n;
+        // int n3 = box[2] / n;
+        // answer = n1 * n2 * n3;
+        int sum = 1;
+        answer = sum;
+        for (int i = 0; i < box.length; i++){
+            answer *= box[i] /n;
+        }
         return answer;
     }
 }
