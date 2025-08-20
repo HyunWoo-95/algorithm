7d1dbc1e70993183bc8b91531dff7c851b212436
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-20 22:56:49 +0900

    [level 0] Title: 배열의 유사도, Time: 1.90 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,9 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
-        List<String> s2List = Arrays.asList(s2);
+        // List<String> s2List = Arrays.asList(s2);
+        // for(String s : s1){
+        //     if(s2List.contains(s)){
+        //         answer++;
+        //     }
+        // }
         for(String s : s1){
-            if(s2List.contains(s)){
+            if(Arrays.stream(s2).anyMatch(s::equals)){
                 answer++;
             }
         }
