3d5a0695d602e29e625bbd28ac39ef499183e58d
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-23 19:21:25 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.75 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,15 +1,10 @@
 class Solution {
     public String solution(String my_string, int n) {
-        // String answer = "";
-        // String[] arr = my_string.split("");
-        // for(int i = 0; i < arr.length; i++){
-        //     answer += arr[i].repeat(n);
-        // }
-        // return answer;
-        StringBuilder sb = new StringBuilder();
-        for(char c : my_string.toCharArray()){
-            sb.append((c + "").repeat(n));
+        String answer = "";
+        String[] arr = my_string.split("");
+        for(int i = 0; i < arr.length; i++){
+            answer += arr[i].repeat(n);
         }
-        return sb.toString();
+        return answer;
     }
 }
