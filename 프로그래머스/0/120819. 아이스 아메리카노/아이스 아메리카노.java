1ca463121fc61470e65061a3922a56ac2237951a
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-23 18:33:12 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-        int coffee = money/ 5500;
-        int change = money - (5500 * coffee);
-        answer[0] = coffee;
-        answer[1] = change;
+        int cost = 5500;
+        answer[0] = money / cost;
+        answer[1] = money - answer[0] * cost; 
         return answer;
     }
 }
