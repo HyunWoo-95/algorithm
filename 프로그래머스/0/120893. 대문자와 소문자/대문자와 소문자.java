874a30673ef6688081372795a5562fc5502f5446
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-24 19:14:46 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.99 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,14 +1,26 @@
+import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        // String answer = "";
+        // for(int i = 0; i < my_string.length(); i++){
+        //     char c= my_string.charAt(i);
+        //     if(Character.isLowerCase(c)){
+        //         answer += (char)(c-32);
+        //     }else{
+        //         answer += (char)(c+32);
+        //     }
+        // }
+        // return answer;
+        StringBuilder sb = new StringBuilder();
+        
         for(int i = 0; i < my_string.length(); i++){
-            char c= my_string.charAt(i);
+            char c = my_string.charAt(i);
             if(Character.isLowerCase(c)){
-                answer += (char)(c-32);
-            }else{
-                answer += (char)(c+32);
+                sb.append(Character.toUpperCase(c));
+            }else {
+                sb.append(Character.toLowerCase(c));
             }
         }
-        return answer;
+        return sb.toString();
     }
 }
