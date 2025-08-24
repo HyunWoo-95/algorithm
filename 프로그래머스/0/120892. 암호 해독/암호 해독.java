df1bd115407db43561b0746d3b4a3a45fdbcb6c1
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-24 18:49:00 +0900

    [level 0] Title: 암호 해독, Time: 3.19 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        String[] arr = cipher.split("");
+        for(int i = code; i <= arr.length; i++){
+            if(i % code == 0){
+                answer += arr[i-1];
+            }
+        }
+        return answer;
+    }
+}
