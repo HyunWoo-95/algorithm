6eb1ce1f18bd707b104e8d34a1f62ae9fb9ab395
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-08-25 22:34:25 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.19 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        String[] arr = my_string.split("");
+        String tmp = "";
+        tmp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = tmp;
+        answer = String.join("", arr);
+        return answer;
+    }
+}
