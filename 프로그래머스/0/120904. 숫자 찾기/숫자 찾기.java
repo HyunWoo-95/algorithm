23a23be2a60e548368e9c49de5622724603904fc
Hyunwoo <108071860+HyunWoo-95@users.noreply.github.com>
2025-09-14 13:49:08 +0900

    [level 0] Title: 숫자 찾기, Time: 5.46 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        String num_str = num + "";
-        String k_str = k + "";
-        if(num_str.contains(k_str)){
-            answer = num_str.indexOf(k_str) + 1;
-        } else {
-            answer = -1;
-        }
-        return answer;
+        // int answer = 0;
+        // String num_str = num + "";
+        // String k_str = k + "";
+        // if(num_str.contains(k_str)){
+        //     answer = num_str.indexOf(k_str) + 1;
+        // } else {
+        //     answer = -1;
+        // }
+        // return answer;
+        return ("-"+num).indexOf(String.valueOf(k));
     }
 }
